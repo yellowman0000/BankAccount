424c668ae9d46ce9c2a25a8804b3ac9677aea231
18687691268 <18687691268@163.com>
2026-02-19 23:36:27 +0700

    Update IBankAccount.java

--- IBankAccount.java
+++ IBankAccount.java
@@ -1,1 +1,11 @@
 //Interface BankAccount
+public interface IBankAccount {
+  
+    void deposit(double amount);
+  
+    boolean withdraw(double amount);
+
+    double getBalance();
+
+    int getPriority();
+}
