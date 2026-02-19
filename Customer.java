ee82a84b83c58c4f411ebd0ee15bc6220c7c4ced
Peerawat Pojlertarun <projectkanya@gmail.com>
2026-02-19 13:42:03 +0700

    Modify Customer class

--- Customer.java
+++ Customer.java
@@ -1,1 +1,12 @@
 // Customer
+public class Customer {
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
