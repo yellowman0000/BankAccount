2daf386bb4df366292df7c0f798d2cb0c8d02a19
Thanapol-6788197 <thanapol.lim@student.mahidol.ac.th>
2026-02-21 19:24:06 +0700

    Update TestBank.java

--- TestBank.java
+++ TestBank.java
@@ -4,30 +4,37 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class TestBank {
+    // Made a queue to hold accounts, will be sorted by priority
     private List<IBankAccount> accountQueue = new ArrayList<>();
-
+    
     public static void main(String[] args) {
+        // Create system and customer
         TestBank engine = new TestBank();
         Customer owner = new Customer("User");
-        
+
+        // Main and savings account
         BankAccount mainAcc = new BankAccount(owner, 1000.0, 1);
         SavingsAccount saveAcc = new SavingsAccount(owner, 500.0, 2, 0.05);
 
+        // Example to show primitive vs reference type
         double testPrimitive = 500.0;
         engine.modifyValues(testPrimitive, mainAcc);
 
         engine.runConcurrentTransactions(mainAcc);
 
+        // Add accounts to the queue and sort
         engine.accountQueue.add(saveAcc);
         engine.accountQueue.add(mainAcc);
         engine.scheduleTransactions();
     }
 
+    
     public void modifyValues(double val, BankAccount acc) {
-        val = val + 1000;
-        acc.deposit(1000);
+        val = val + 1000; // Primitive values don't actually change
+        acc.deposit(1000); // Update the actual variable
     }
 
+    // Uses multiple threads to deposit money at the same time
     public void runConcurrentTransactions(BankAccount acc) {
         ExecutorService executor = Executors.newFixedThreadPool(3); 
 
@@ -36,11 +43,13 @@ public class TestBank {
                 acc.deposit(10.0);
             }
         };
-
+        
+        //Execute same tasks in 3 threads
         executor.execute(loginAndDeposit);
         executor.execute(loginAndDeposit);
         executor.execute(loginAndDeposit);
 
+        // Shuts down the executor and wait for all tasks to complete
         executor.shutdown();
         try {
             if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
@@ -51,6 +60,7 @@ public class TestBank {
         }
     }
 
+    // Sort accounts by priority, and prints balance
     public void scheduleTransactions() {
         accountQueue.sort(Comparator.comparingInt(IBankAccount::getPriority));
         for (IBankAccount acc : accountQueue) {
