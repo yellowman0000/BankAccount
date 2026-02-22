878919e71259697491843ab1075df76f4ac01940
atulyado <atulyado198@gmail.com>
2026-02-22 13:38:58 +0700

    Added SavingsAccount class

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,1 +1,40 @@
 // SavingsAccount
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(Customer owner, double initialBalance, int priority, double interestRate) {
+        super(owner, initialBalance, priority);
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        double interestAmount = getBalance() * interestRate;
+        deposit(interestAmount);
+        System.out.println("Interest applied: $" + interestAmount);
+    }
+
+    public void triggerOverflow() {
+        System.out.println("Starting stack overflow demonstration...");
+        recursiveCall(1);
+    }
+    
+    private void recursiveCall(int depth) {
+        System.out.println("Stack depth: " + depth);
+        recursiveCall(depth + 1);
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Account Type: Savings Account");
+        System.out.println("Interest Rate: " + (interestRate * 100) + "%");
+    }
+}
